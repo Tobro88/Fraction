41116216c13f4186db9548503f1953bac099fbca
Tobro88 <tobro@outlook.com>
2015-03-09 23:00:38 +0100

    Radical revamp and clean up

--- src/fractiondemo/Fraction.java
+++ src/fractiondemo/Fraction.java
@@ -1,228 +1,112 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fractiondemo;
 
-/**
- * @author Tobro88
- */
 public class Fraction {
 
     private int numerator;
     private int denominator;
-    private int greatestCommonDivisor = 0;
-    private int reducedNumerator;
-    private int reducedDenominator;
 
-    /**
-     * Constructor with defined numerator and denominator values. Denominator can not be set to zero. If
-     * <code>denominatorArgument</code> is zero the Fraction is initialized with numerator = 0 and denominator =1.
-     *
-     * In the constructor, also GCD is determined and the Reduced Numerator and Reduced Denominator are calculated.
-     *
-     * @param numeratorArgument the numerator of the fraction
-     * @param denominatorArgument the denominator of the fraction
-     * @author Tobro88
-     */
-    public Fraction(int numeratorArgument, int denominatorArgument) {
-        // Here comes the constructor
-
-        if (denominatorArgument == 0) {
-            //System.out.println("Denominator can not be zero.");
-            //System.out.println("Numerator set to 0,");
-            //System.out.println("and Denominator set to 1");
-            this.numerator = 0;
-            this.denominator = 1;
-            this.greatestCommonDivisor = this.gcdCalculation(0, 1);
-            this.reducedNumerator = 0;
-            this.reducedDenominator = 1;
-            return;
-        }
-        this.numerator = numeratorArgument;
-        this.denominator = denominatorArgument;
-        this.greatestCommonDivisor = this.gcdCalculation(numeratorArgument,
-                denominatorArgument);
-        this.reducedNumerator = this.numerator / this.greatestCommonDivisor;
-        this.reducedDenominator = this.denominator / this.greatestCommonDivisor;
-    }
-
-    /**
-     * Constructor without initial numerator and denominator values. Default value for numerator = 0, and for
-     * denominator = 1.
-     *
-     * In the constructor, also GCD is determined and the Reduced Numerator and Reduced Denominator are calculated.
-     * Since numerator = 0 and denominator = 1 GCD is 1.
-     *
-     * @author Tobro88
-     */
     public Fraction() {
-        // Constructor in case no numerator/denominator is given yet
         this.numerator = 0;
         this.denominator = 1;
-        this.greatestCommonDivisor = this.gcdCalculation(0, 1);
-        this.reducedNumerator = 0;
-        this.reducedDenominator = 1;
+        this.isReduced = true;
+    }
+
+    public Fraction(int numeratorArgument, int denominatorArgument) {
+
+        if (denominatorArgument == 0) {
+            numerator = 0;
+            denominator = 1;
+            return;
+        }
+        numerator = numeratorArgument;
+        denominator = denominatorArgument;
+    }
+
+    public Fraction(Fraction sourceFraction) {
+
+        if (sourceFraction == null) {
+            System.exit(0);
+        }
+
+        numerator = sourceFraction.numerator;
+        denominator = sourceFraction.denominator;
 
     }
 
-    /**
-     * @author Tobro88 true@return
-     * @return
-     */
-    public int getNumerator() {
-        return this.numerator;
-    }
-
-    /**
-     * @author Tobro88
-     * @return
-     */
-    public int getDenominator() {
-        return this.denominator;
-    }
-
-    /**
-     * @author Tobro88
-     * @return
-     */
-    public int getGCD() {
-        return this.greatestCommonDivisor;
-    }
-
-    /**
-     * @author Tobro88
-     * @return
-     */
-    public int getReducedNumerator() {
-        return this.reducedNumerator;
-    }
-
-    /**
-     * @author Tobro88
-     * @return
-     */
-    public int getReducedDenominator() {
-        return this.reducedDenominator;
-    }
-
-    /**
-     * Compares this Fraction with another Fraction based on the reduced numerator and denominator. For example the
-     * method returns <code>true</code> if 2/4 is compared with 1/2.
-     *
-     * @param otherFraction the fraction object with which to compare
-     * @author Tobro88
-     * @return <code>true</code> if this fraction is the same in reduced form as <code>otherFraction</code> in reduced
-     * form
-     */
     public boolean isEqualInReducedForm(Fraction otherFraction) {
 
-        return ((this.reducedNumerator == otherFraction.reducedNumerator)
-                && (this.reducedDenominator
-                == otherFraction.reducedDenominator));
+        Fraction thisFractionInComparison = new Fraction(this);
+        thisFractionInComparison.reduce();
+
+        Fraction otherFractionInComparison = new Fraction(otherFraction);
+        otherFractionInComparison.reduce();
+
+        return equals(thisFractionInComparison, otherFractionInComparison);
+
     }
 
-    /**
-     * @param numeratorArgument
-     * @author Tobro88
-     */
-    public void setNumerator(int numeratorArgument) {
-        //if Numerator is changed other instances also need to be updated
-
-        this.numerator = numeratorArgument;
-        this.greatestCommonDivisor
-                = this.gcdCalculation(this.numerator, this.denominator);
-        this.reducedNumerator = this.numerator / this.greatestCommonDivisor;
-        this.reducedDenominator = this.denominator / this.greatestCommonDivisor;
+    public boolean equals(Fraction fractionA, Fraction fractionB) {
+        return ((fractionA.numerator == fractionB.numerator) && (fractionA.denominator == fractionB.denominator));
     }
 
-    /**
-     * @param denominatorArgument
-     * @author Tobro88
-     */
-    public void setDenominator(int denominatorArgument) {
-        //if Denominator is changed other instances also need to be updated
-
-        this.denominator = denominatorArgument;
-        this.greatestCommonDivisor
-                = this.gcdCalculation(this.numerator, this.denominator);
-        this.reducedNumerator = this.numerator / this.greatestCommonDivisor;
-        this.reducedDenominator = this.denominator / this.greatestCommonDivisor;
+    public boolean isReduced() {
+        return (gcdCalculation(numerator, denominator) == 1);
     }
 
-    /**
-     * @author Tobro88
-     * @return
-     */
     public double decimalValueOfFraction() {
         return (double) this.numerator / (double) this.denominator;
     }
 
-    /**
-     * Returns the result (as a reduced <code>Fraction</code> object) of adding otherFraction to the class'
-     * instantiation.
-     *
-     * @param otherFraction the Fraction that is added
-     * @author Tobro88
-     * @return
-     */
     public Fraction reducedAdd(Fraction otherFraction) {
 
-        Fraction resultFraction = new Fraction();
-
-
-        resultFraction.setNumerator(this.numerator * otherFraction.denominator
+        int resultNumerator = (this.numerator * otherFraction.denominator
                 + this.denominator * otherFraction.numerator);
-        resultFraction.setDenominator(this.denominator * otherFraction.denominator);
-        return new Fraction(resultFraction.reducedNumerator,resultFraction.reducedNumerator);
+        int resultDenominator = (this.denominator * otherFraction.denominator);
+        Fraction resultFraction = new Fraction(resultNumerator, resultDenominator);
+        resultFraction.reduce();
+        return resultFraction;
     }
 
-    /**
-     * Returns the result (as a reduced <code>Fraction</code> object) of subtracting otherFraction from the class'
-     * instantiation.
-     *
-     * @param otherFraction
-     * @author Tobro88
-     * @return
-     */
     public Fraction reducedSubtract(Fraction otherFraction) {
         //method to subtract another Fraction from Fraction and reduce result
         //current method is obviously a placeholder
-        Fraction resultFraction = new Fraction();
-
-
-        resultFraction.setNumerator(this.numerator * otherFraction.denominator
+        int resultNumerator = (this.numerator * otherFraction.denominator
                 - this.denominator * otherFraction.numerator);
-        resultFraction.setDenominator(this.denominator * otherFraction.denominator);
-        return new Fraction(resultFraction.reducedNumerator,resultFraction.reducedNumerator);
+        int resultDenominator = (this.denominator * otherFraction.denominator);
+        Fraction resultFraction = new Fraction(resultNumerator, resultDenominator);
+        resultFraction.reduce();
+        return resultFraction;
     }
 
-    /**
-     * Returns the result (as a reduced <code>Fraction</code> object) of multiplying otherFraction to the class'
-     * instantiation.
-     *
-     * @param otherFraction the Fraction that is added
-     * @author Tobro88
-     * @return
-     */
     public Fraction reducedMultiply(Fraction otherFraction) {
 
-        Fraction resultFraction = new Fraction();
-
-
-        resultFraction.setNumerator(this.numerator * otherFraction.numerator);
-        resultFraction.setDenominator(this.denominator * otherFraction.denominator);
-
-        return new Fraction(resultFraction.reducedNumerator,resultFraction.reducedNumerator);
+        int resultNumerator = (this.numerator * otherFraction.numerator);
+        int resultDenominator = (this.denominator * otherFraction.denominator);
+        Fraction resultFraction = new Fraction(resultNumerator, resultDenominator);
+        resultFraction.reduce();
+        return resultFraction;
     }
 
-    /**
-     * @param n1
-     * @param n2
-     * @author Tobro88
-     * @return
-     */
+    public Fraction reducedDivide(Fraction otherFraction) {
+        int resultNumerator = (this.numerator * otherFraction.denominator);
+        int resultDenominator = (this.denominator * otherFraction.numerator);
+        Fraction resultFraction = new Fraction(resultNumerator, resultDenominator);
+        resultFraction.reduce();
+        return resultFraction;
+    }
+
+    public void reduce() {
+        int gcd = gcdCalculation(numerator, denominator);
+        numerator = numerator / gcd;
+        denominator = denominator / gcd;
+    }
+
+    public void invert() {
+        int temp = numerator;
+        numerator = denominator;
+        denominator = temp;
+    }
+
     private int gcdCalculation(int n1, int n2) { //Euclidean algorithm
         if (n2 == 0) {
             return n1;
